6510ce25c5311cef9a2e83be88538825ddfb046e
nilsonvalimjr <nilson.valim@redspark.io>
2020-12-06 19:33:27 -0300

    Add same validation on dtos

--- src/main/kotlin/com/donus/donuscodechallenge/account/controller/request/DepositRequest.kt
+++ src/main/kotlin/com/donus/donuscodechallenge/account/controller/request/DepositRequest.kt
@@ -1,9 +1,13 @@
 package com.donus.donuscodechallenge.account.controller.request
 
+import com.sun.istack.NotNull
+import org.hibernate.validator.constraints.NotBlank
 import java.math.BigDecimal
 import java.util.*
 
 data class DepositRequest(
-    val accountId: UUID,
-    val amount: BigDecimal
+        @field:NotNull
+        val accountId: UUID,
+        @field:NotNull
+        val amount: BigDecimal
 ) : BaseTransactionRequest
--- src/main/kotlin/com/donus/donuscodechallenge/account/controller/request/TransferRequest.kt
+++ src/main/kotlin/com/donus/donuscodechallenge/account/controller/request/TransferRequest.kt
@@ -1,10 +1,15 @@
 package com.donus.donuscodechallenge.account.controller.request
 
+import com.sun.istack.NotNull
+import org.hibernate.validator.constraints.NotBlank
 import java.math.BigDecimal
 import java.util.*
 
 data class TransferRequest(
+        @field:NotNull
         val accountId: UUID,
+        @field:NotNull
         val destinationAccountId: UUID,
+        @field:NotNull
         val amount: BigDecimal
 ) : BaseTransactionRequest
